c7c3e8b0fcf93a04c15f9a20ea1779b1063833cd
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-04 18:28:25 +0200

    Segunda correccao adicionada

---
+++ OlaMundo/src/olamundo/OlaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olamundo;
+ 
+import javax.swing.JOptionPane;
+/*
+ * @author Filipe Emanuel
+ */
+public class OlaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ola, Mundo!");
+        JOptionPane.showMessageDialog(null, "Ola, Mundo!");
+    }
+    
+}
