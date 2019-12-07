9c1709fbfe028afcaf4f3029a26cf08bdda253cb
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 14:33:26 +0200

    Resolução do Exercício 14e.

--- CursoJava16h2019/src/mz/e/aula03/Exercise14e.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise14e.java
@@ -1,14 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 14.	Escreva uma classe ConversaoDeUnidadesDeTemperatura que contenha métodos estáticos para 
+        calcular a conversão entre diferentes escalas de temperatura. Considere as fórmulas de conversão abaixo: 
+    e)	De graus Celsius (C) para graus Réaumur (Re): Re = C ∗ 4/5 
  */
 package mz.e.aula03;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Filipe Emanuel
  */
 public class Exercise14e {
-    
+    public static void main(String [] arg){
+       Scanner scanner = new Scanner(System.in);
+        double t_em_c, t_em_Reumur;
+        
+        System.out.print("Digite temperatura em Celsius: ");
+        t_em_c = scanner.nextDouble();
+        
+        t_em_Reumur = t_em_c * 4/5;
+        
+        System.out.println("Temperatura em Réaumur (Re): "+t_em_Reumur); 
+    }
 }
