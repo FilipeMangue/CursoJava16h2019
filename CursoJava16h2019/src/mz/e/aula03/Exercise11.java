8df3dd6c38c91c44cb8dd7aa364d014fe9bd3cf7
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 13:40:11 +0200

    Resolução do Exercício 11.

--- CursoJava16h2019/src/mz/e/aula03/Exercise11.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise11.java
@@ -1,9 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    11.	Crie um programa que calcule a média de um aluno para a disciplina de programação. 
+        O aluno deve informar seu nome e as três notas da matéria.
  */
 package mz.e.aula03;
+
+import java.util.Scanner;
 
 /**
  *
@@ -12,5 +13,23 @@ package mz.e.aula03;
  * @author José Seie      |
  */
 public class Exercise11 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double nota1, nota2 = 0, nota3 = 0, media, soma;
+        
+        System.out.print("Digite nota 1: ");
+        nota1 = scan.nextDouble();
+        
+        System.out.print("Digite nota 1: ");
+        nota2 = scan.nextDouble();
+        
+        System.out.print("Digite nota 1: ");
+        nota3 = scan.nextDouble();
+        
+        soma = nota1 + nota2 + nota3;
+        
+        media = soma / 3;
+        
+        System.out.println("A média  dos testes é "+media);
+    }
 }
