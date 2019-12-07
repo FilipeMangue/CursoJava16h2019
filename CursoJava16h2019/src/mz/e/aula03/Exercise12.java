9a26a0378b668c1f4fd8015b4c5bdb7425a9162e
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 13:57:04 +0200

    Resolução do Exercício 12.

--- CursoJava16h2019/src/mz/e/aula03/Exercise12.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise12.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    12.	Fazer um programa que imprima a média aritmética dos números 8,9 e 7. 
+        A média dos números 4, 5 e 6. A soma das duas médias. A média das médias.
  */
 package mz.e.aula03;
 
@@ -12,5 +11,17 @@ package mz.e.aula03;
  * @author José Seie      |
  */
 public class Exercise12 {
-    
+    public static void main(String[] args) {
+        double soma1, soma2, media1, media2, media_das_medias;
+        
+        soma1 = 8 + 9 + 7;
+        soma2 = 4 + 5 + 6;
+        
+        media1 = soma1/3;
+        media2 = soma2/3;
+        
+        media_das_medias = (media1 + media2)/2;
+        
+        System.out.println("A média1: "+media1+", média2: "+media2+" e a média das médias: "+media_das_medias);
+    }
 }
