ba6601cb95dc679e0a846af600169e99fd683b63
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 14:03:01 +0200

    Resolução do Exercício 13.

--- CursoJava16h2019/src/mz/e/aula03/Exercise13.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise13.java
@@ -1,9 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    13.	Informar um saldo e imprimir o saldo com o aumento de 1%.
  */
 package mz.e.aula03;
+
+import java.util.Scanner;
 
 /**
  *
@@ -12,5 +12,16 @@ package mz.e.aula03;
  * @author José Seie      |
  */
 public class Exercise13 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double saldo, saldo_aumento, percentagem = 0.01;
+
+        System.out.print("Informe um saldo: ");
+        saldo = scan.nextDouble();
+        
+        saldo_aumento = saldo + (saldo * percentagem);
+        
+        System.out.println("Saldo de normal: "+saldo+"\nSaldo com 1 por cento de aumento: "+saldo_aumento);
+        
+    }
 }
