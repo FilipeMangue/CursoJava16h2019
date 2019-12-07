88f12c5490b1942720c4261678ccb18a71c42fd5
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 13:33:47 +0200

    Resolução do Exercício10.

--- CursoJava16h2019/src/mz/e/aula03/Exercise10.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise10.java
@@ -1,9 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    10.	Faça um algoritmo que leia dois números e mostre qual o maior dos dois
  */
 package mz.e.aula03;
+
+import java.util.Scanner;
 
 /**
  *
@@ -12,5 +12,19 @@ package mz.e.aula03;
  * @author José Seie      |
  */
 public class Exercise10 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double num1, num2;
+        
+        System.out.print("Digite um número 1: ");
+        num1 = scan.nextInt();
+        System.out.print("Digite um número 2: ");
+        num2 = scan.nextInt();
+        
+        if (num1 > num2) {
+            System.out.println("O número 1("+num1+") é o Maior.");
+        } else if (num1 < num2) {
+            System.out.println("O número 2("+num2+") é o Maior.");
+        }
+    }
 }
