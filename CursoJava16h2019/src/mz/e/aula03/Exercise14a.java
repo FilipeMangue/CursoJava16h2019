78b93e0ca00dfd20f09727d2497eb6eb10b2b000
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 14:22:26 +0200

    Resolução do Exercício 14a.

--- CursoJava16h2019/src/mz/e/aula03/Exercise14a.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise14a.java
@@ -1,14 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 14.	Escreva uma classe ConversaoDeUnidadesDeTemperatura que contenha métodos estáticos para 
+        calcular a conversão entre diferentes escalas de temperatura. Considere as fórmulas de conversão abaixo: 
+    a)	De graus Celsius (C) para graus Fahrenheit (F): F = (9 × C/5) + 32) 
  */
 package mz.e.aula03;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Filipe Emanuel
  */
 public class Exercise14a {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double t_em_c, t_em_fahr;
+        
+        System.out.print("Digite temperatura em Celsius: ");
+        t_em_c = scanner.nextDouble();
+        
+        t_em_fahr = (9 * t_em_c/5) + 32;
+        
+        System.out.println("Temperatura em Fahreiheit: "+t_em_fahr);
+    }
 }
