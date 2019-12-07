bf2840674911c5e307fe6a6a98d80bddee54d29b
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 12:26:21 +0200

    Resolução do Exercício 02.

--- CursoJava16h2019/src/mz/e/aula03/Exercise02.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise02.java
@@ -1,10 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+2.Faça um programa que imprima a frase: "Seja bem-vindo ao meu programa!", 
+  Em seguida, imprima uma linha em branco e na sequência, imprima as frases 
+  e o resultado da expressões matemáticas.
+  “O produto de 8 por 90 é igual a:”
+  “O quociente de 90 por 30 é igual a:”
+  “O quadrado de 9 é: “
+  Cada frase e seu resultado devem estar na mesma linha.
  */
 package mz.e.aula03;
-
 /**
  *
  * @author Filipe Mangue  | 
@@ -12,5 +15,15 @@ package mz.e.aula03;
  * @author José Seie      |
  */
 public class Exercise02 {
-    
+    public static void main(String[] args) {
+        int num1 = 90, num2 = 8, num3 = 30, num4 = 9;
+        int produto, quociente, quadrado;
+        produto = num2 + num1;
+        quociente = num1 + num3;
+        quadrado = num4^2;
+        System.out.println("Seja bem-vindo ao meu programa!\n");
+        System.out.println("O produto de 8 por 90 é igual a: "+produto);
+        System.out.println("O quociente de 90 por 30 é igual a: "+quociente);
+        System.out.println("O quadrado de 9 é: "+quadrado);
+    }
 }
