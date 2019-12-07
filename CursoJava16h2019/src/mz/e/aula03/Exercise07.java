db2d6eb9125417bed6f5ae45b182cebc4a7ae4c3
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 12:58:51 +0200

    Resolução do Exercício 07.

--- CursoJava16h2019/src/mz/e/aula03/Exercise07.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise07.java
@@ -1,9 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    7.	Faça um programa que leia os valores A e B. Mostre uma mensagem que informe se a soma
+    de A com B e igual a A.
  */
 package mz.e.aula03;
+
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -12,5 +13,17 @@ package mz.e.aula03;
  * @author José Seie      |
  */
 public class Exercise07 {
-    
+    public static void main(String[] args) {
+        double a,b,soma;
+        
+        a = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite um valor A: "));
+        b = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite um valor A: "));
+        
+        soma = a + b;
+        if (soma != a) {
+            JOptionPane.showMessageDialog(null, "A Soma de A e B: "+soma+" não é igual a  A: "+a);
+        } else {
+            JOptionPane.showMessageDialog(null, "A Soma de A e B: "+soma+" é igual a  A: "+a);
+        } 
+    }
 }
