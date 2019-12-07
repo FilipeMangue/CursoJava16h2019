633c657491c4ef98c0c9e26c2917f056608955b6
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 14:37:51 +0200

    Resolução do Exercício 14g.

--- CursoJava16h2019/src/mz/e/aula03/Exercise14g.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise14g.java
@@ -1,14 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 14.	Escreva uma classe ConversaoDeUnidadesDeTemperatura que contenha métodos estáticos para 
+        calcular a conversão entre diferentes escalas de temperatura. Considere as fórmulas de conversão abaixo: 
+    g)	De graus Kelvin (K) para graus Rankine (R): R = K ∗ 1.8 
  */
 package mz.e.aula03;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Filipe Emanuel
  */
 public class Exercise14g {
-    
+    public static void main(String [] arg){
+       Scanner scanner = new Scanner(System.in);
+        double t_emK, t_emRa;
+        
+        System.out.print("Digite temperatura em  Kelvin (K): ");
+        t_emK = scanner.nextDouble();
+        
+        t_emRa = t_emK * 1.8;
+        
+        System.out.println("Temperatura em Rankine (R): "+t_emRa); 
+    }
 }
