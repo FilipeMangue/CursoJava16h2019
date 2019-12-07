9d959439f7f5a597b93aba510cf665ef425f9fec
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 14:30:32 +0200

    Resolução do Exercício 14d.

--- CursoJava16h2019/src/mz/e/aula03/Exercise14d.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise14d.java
@@ -1,14 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 14.	Escreva uma classe ConversaoDeUnidadesDeTemperatura que contenha métodos estáticos para 
+        calcular a conversão entre diferentes escalas de temperatura. Considere as fórmulas de conversão abaixo:  
+    d)	De graus Kelvin (K) para graus Celsius (C): C = K − 273.15 
  */
 package mz.e.aula03;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Filipe Emanuel
  */
 public class Exercise14d {
-    
+    public static void main(String [] arg){
+       Scanner scanner = new Scanner(System.in);
+        double t_em_c, t_emK;
+        
+        System.out.print("Digite temperatura em Celsius: ");
+        t_emK = scanner.nextDouble();
+        
+        t_em_c = t_emK - 273.15;
+        
+        System.out.println("Temperatura em Kelvin: "+t_em_c); 
+    }
 }
