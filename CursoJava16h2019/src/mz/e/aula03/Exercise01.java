5db8977c89ae37a6680b8069a8297b90a4ba16c0
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 12:08:51 +0200

    Resolução do Exercício 01.

--- CursoJava16h2019/src/mz/e/aula03/Exercise01.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise01.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 1. Faça um programa que imprima o texto “Olá  mundo" para o usuário. Em seguida, imprima na 
+    tela o seu código de visita da plataforma Explicador e então, imprima uma linha em branco. 
+    Depois de imprimir a linha em branco, escreva na tela a seguinte frase:
+    "Seja bem-vindo ao curso de Java Básico Intensivo"
  */
 package mz.e.aula03;
 
@@ -12,5 +13,7 @@ package mz.e.aula03;
  * @author José Seie      |
  */
 public class Exercise01 {
-    
+    public static void main(String[] args) {
+        System.out.println("Olá  mundo.\nCódigo de visita na plataforma Explicador: #334\n\nSeja bem-vindo ao curso de Java Básico Intensivo");
+    }
 }
