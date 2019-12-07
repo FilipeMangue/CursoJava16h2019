d655069850642e9ece8d1ba13d6832abf5956abe
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 12:34:52 +0200

    Resolução do Exercício 03.

--- CursoJava16h2019/src/mz/e/aula03/Exercise03.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise03.java
@@ -1,9 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+3.  Escreva um pequeno programa que peça o seu nome, sua ocupação e ano de nascimento. Em seguida, 
+    imprima na tela as 2 informações a seguir:
+    Eu chamo me xxxxx,  sou xxxxx e tenho xxxxx anos de idade.
  */
 package mz.e.aula03;
+
+import java.util.Scanner;
 
 /**
  *
@@ -12,5 +14,20 @@ package mz.e.aula03;
  * @author José Seie      |
  */
 public class Exercise03 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nome, ocupacao;
+        int ano_nasc, idade;
+        
+        System.out.print("Digite seu nome: ");
+        nome = scan.nextLine();
+        System.out.print("Sua ocupação: ");
+        ocupacao = scan.nextLine();
+        System.out.print("Digite seu ano de nascimento: ");
+        ano_nasc = scan.nextInt();
+        
+        idade = 2019 - ano_nasc;
+        
+        System.out.println("Eu chamo me "+nome+",  sou "+ocupacao+" e tenho "+idade+" anos de idade.");
+    }
 }
