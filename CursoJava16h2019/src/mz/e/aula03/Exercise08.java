fd31bd981719fdc4e341b0f31dc839e4d8846384
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 13:18:44 +0200

    Resolução do Exercício 08.

--- CursoJava16h2019/src/mz/e/aula03/Exercise08.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise08.java
@@ -1,9 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    8.	Desenvolva um programa que leia um número inteiro e imprima o seu antecessor e seu sucessor.
  */
 package mz.e.aula03;
+
+import java.util.Scanner;
 
 /**
  *
@@ -12,5 +12,16 @@ package mz.e.aula03;
  * @author José Seie      |
  */
 public class Exercise08 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num, antecessor, sucessor;
+        
+        System.out.println("Digite um número inteiro: ");
+        num = scan.nextInt();
+        
+        antecessor = num - 1; 
+        sucessor = num + 1;
+        
+        System.out.println("O antecessor de "+num+" é "+antecessor+" e o seu sucessor é "+sucessor);
+    }
 }
