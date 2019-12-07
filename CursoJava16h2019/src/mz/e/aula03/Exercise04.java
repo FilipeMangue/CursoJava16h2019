1f0a2d3048be2675d4ae7aebd91662b44ce05764
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-07 12:39:25 +0200

    Resolução do Exercício 04.

--- CursoJava16h2019/src/mz/e/aula03/Exercise04.java
+++ CursoJava16h2019/src/mz/e/aula03/Exercise04.java
@@ -1,9 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+   4. Faça um programa que leia o seu nome e imprima em tela
  */
 package mz.e.aula03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -12,5 +14,11 @@ package mz.e.aula03;
  * @author José Seie      |
  */
 public class Exercise04 {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String nome;
+        System.out.print("Digite seu nome: ");
+        nome = br.readLine();
+        System.out.print("Nome: "+nome);
+    }
 }
