5ea84c708c42046f70762156c7ee753be0980db8
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-10 19:18:53 +0200

    Resolucao do Exercicio 16

--- CursoJava16h2019/src/correccaoExercAula07/Exerc16.java
+++ CursoJava16h2019/src/correccaoExercAula07/Exerc16.java
@@ -5,13 +5,30 @@
  */
 package correccaoExercAula07;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author José Seie
  * @author Filipe Mangue
  */
 public class Exerc16 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] a = new int[15];
+        int[] b = new int[15];
         
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Digite o valor ("+i+"): ");
+            a[i] =  Integer.parseInt(br.readLine());
+        }
+        for (int i = 0; i < 15; i++) {
+            b[i] = a[i] * a[i];
+        }
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Valor B("+i+"): "+b[i]);
+        }
     }
 }
