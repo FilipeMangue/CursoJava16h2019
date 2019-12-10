9468a565c108c3dd970d04f099be679cfe759fb6
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-10 18:59:25 +0200

    Resolucao do esxercicio 17

--- CursoJava16h2019/src/correccaoExercAula07/Exerc17.java
+++ CursoJava16h2019/src/correccaoExercAula07/Exerc17.java
@@ -1,14 +1,39 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+17. Arrays A e  B  cada um com 10  elementos  inteiros. Construir um array C, 
+    Criar dois onde cada elemento de  C  é a divisão dos  respectivos  elementos  
+    em  A e  B,  ou seja: C[i] = A[i] / float(B[i]).
  */
 package correccaoExercAula07;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
- * @author Filipe Emanuel
+ * @author José Seie
+ * @author Filipe Mangue
  */
 public class Exerc17 {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] a = new int[10];
+        int[] b = new int[10];
+        int[] c = new int[10];
+        
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Digite o valor A ("+i+"): ");
+            a[i] =  Integer.parseInt(br.readLine());
+        }
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Digite o valor B ("+i+"): ");
+            b[i] =  Integer.parseInt(br.readLine());
+        }
+        for (int i = 0; i < 15; i++) {
+            c[i] = a[i] / b[i];
+        }
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Valor C("+i+"): "+c[i]);
+        }
+    }
 }
