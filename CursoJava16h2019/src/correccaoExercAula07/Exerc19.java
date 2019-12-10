4e6711d4f1d3167b3767d519b75985569d62c40f
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-10 19:18:23 +0200

    Resolucao do exercicio 19

--- CursoJava16h2019/src/correccaoExercAula07/Exerc19.java
+++ CursoJava16h2019/src/correccaoExercAula07/Exerc19.java
@@ -1,16 +1,45 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Faça um programa de consulta de uma Explicadores chave de  dados:  
+    leia nomes  de  pessoas  co plataforma Explicador , sendo a m a partir 
+    de um  nome informado por seus  respectivos códigos  de visita na 
+    quantidade determinada pelo usuário. Em seguida pergunte ao  usuário  
+    qual o nome  que ele deseja  consultar o resposta,  exiba  o código  
+    do explicador procurado . código .  Após  sua resposta,  exiba  o código  
+do explicador procurado
  */
 package correccaoExercAula07;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Filipe Emanuel
+ * @author José Seie
+ * @author Filipe Mangue
  */
 public class Exerc19 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int qtd, numero;
+        String nome;
         
+        System.out.println("Digite a quantidade: ");
+        qtd = scanner.nextInt();
+         
+        String[] nomes = new String[qtd];
+        String[] codigos = new String[qtd];
+        
+        for(int i = 0; i < qtd; i++){
+            System.out.println("Digite nome("+i+"): ");
+            nomes[i] = scanner.next();
+            System.out.println("Digite o codigo("+i+"): ");
+            codigos[i] = scanner.next();
+        }
+        System.out.println("Que nome deseja consultar: ");
+        nome = scanner.next();
+        for(int i = 0; i < qtd; i++){
+            if(nome.equals(nomes[i])) {
+                System.out.println("Codigo de Visita: "+codigos[i]); 
+            }
+        }
     }
 }
