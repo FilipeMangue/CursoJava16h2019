f46500bd386cbcb04e93fb48371ab092d7e79445
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-10 19:09:24 +0200

    Resolucao do Exercicio 18

--- CursoJava16h2019/src/correccaoExercAula07/Exerc18.java
+++ CursoJava16h2019/src/correccaoExercAula07/Exerc18.java
@@ -1,14 +1,40 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+18. Faça um programa de consulta pela  posição pessoas, numérica da pessoa:  
+    leia nomes  de sendo  a quantidade determinada pelo  usuário. Logo após  a 
+    entrada  pergunte ao usuário  o número  do  nome que ele gostaria de consultar.  
+    Após  sua resposta,  exiba o nome que fica na  posição informada. Chame atenção  
+    do usuário em caso de  uma consulta inválida,  ou seja,  com  números menores  ou  
+    iguais  a  zero, ou maiores  do  que a  quantidade cadastrada.
  */
 package correccaoExercAula07;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Filipe Emanuel
+ * @author José Seie
+ * @author Filipe Mangue
  */
 public class Exerc18 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int qtd, numero;
+        
+        System.out.println("Digite a quantidade: ");
+        qtd = scanner.nextInt();
+         
+        String[] nomes = new String[qtd];
+        
+        for(int i = 0; i < qtd; i++){
+            System.out.println("Digite nome("+i+"): ");
+            nomes[i] = scanner.next();
+        }
+        System.out.println("Digite o número  do  nome que deseja consultar: ");
+        numero = scanner.nextInt();
+        for(int i = 0; i < qtd; i++){
+            if(i == numero){
+                System.out.println("Nome: "+nomes[i]); 
+            }
+        }
+    }
 }
