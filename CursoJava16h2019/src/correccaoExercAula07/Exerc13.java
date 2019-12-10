18270f5b65b04cb66c40dad42f0a46f0b4e50bcf
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-10 18:37:45 +0200

    Resolucao do exercicio 13

--- CursoJava16h2019/src/correccaoExercAula07/Exerc13.java
+++ CursoJava16h2019/src/correccaoExercAula07/Exerc13.java
@@ -1,14 +1,31 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+13. Criar um array A com 10 elementos inteiros. Implementar um programa que 
+    defina e escreva a quantidade de elementos armazenados neste vetor que são pares. 
  */
 package correccaoExercAula07;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
- * @author Filipe Emanuel
+ * @author José Seie
+ * @author Filipe Mangue
  */
 public class Exerc13 {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] valor = new int[5];
+        int qtdPares = 0;
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite o valor ("+i+"): ");
+            valor[i] = Integer.parseInt(br.readLine());
+            if(valor[i] % 2 == 0){
+                qtdPares += 1;
+            }
+        }
+        System.out.println("Quantidade de pares: "+qtdPares);
+    }
 }
