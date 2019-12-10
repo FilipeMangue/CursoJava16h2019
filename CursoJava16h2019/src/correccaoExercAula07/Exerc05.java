f0418bd0d2cb512301ed965f69e54f44c11b6208
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-10 16:59:37 +0200

    Resolucao Exercicio 05

---
+++ CursoJava16h2019/src/correccaoExercAula07/Exerc05.java
@@ -0,0 +1,23 @@
+/*
+3. Faça um algoritmo que some o conteúdo de dois arrays 
+   e armazene o resultado em um terceiro array. 
+ */
+package correccaoExercAula07;
+
+/**
+ *
+ * @author José Seie
+ * @author Filipe Mangue
+ */
+public class Exerc05 {
+    public static void main(String[] args) {
+        int [] a = new int []{1,1,1,1,1,1,1,1};
+        int [] b = new int []{1,1,1,1,1,1,1,1};
+        int [] c = new int [8];
+        
+        for (int i = 0; i < 5; i++) {
+            c[i] = a[i] + b[i];
+            System.out.println(" "+c[i]);
+        }
+    }
+}
