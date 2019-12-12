b780fea1bfd56f5b9c05fdc9a46ff5e01a459438
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-12 16:11:19 +0200

    Adição dos Atributos

--- CursoJava16h2019/src/correccaoExercAula08/exercicio12/Endereco.java
+++ CursoJava16h2019/src/correccaoExercAula08/exercicio12/Endereco.java
@@ -10,5 +10,9 @@ package correccaoExercAula08.exercicio12;
  * @author Filipe Emanuel
  */
 public class Endereco {
-    
+    String rua;
+    int numero;
+    String bairro;
+    String cidade;
+    String provincia;
 }
