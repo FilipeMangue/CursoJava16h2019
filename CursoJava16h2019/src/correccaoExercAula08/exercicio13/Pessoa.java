2139e349a7ba192c810fa7e11e0aed039ea14b7b
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-12 15:52:18 +0200

    Adição de Atributos da Classe

--- CursoJava16h2019/src/correccaoExercAula08/exercicio13/Pessoa.java
+++ CursoJava16h2019/src/correccaoExercAula08/exercicio13/Pessoa.java
@@ -10,6 +10,12 @@ package correccaoExercAula08.exercicio13;
  * @author Filipe Emanuel
  */
 public class Pessoa {
+    String rua;
+    int numero;
+    String bairro;
+    String cidade;
+    String provincia;
+    
     public void opcoes(){
         System.out.println("1 - Cadastrar Pessoa\n"
                          + "2 - Remover Pessoa"
