7f5a4927f25a406a1ca129d28d387d6675822f72
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-12 14:41:06 +0200

    Adição da Classe Lâmpada

---
+++ CursoJava16h2019/src/correccaoExercAula08/Lampada.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package correccaoExercAula08;
+
+/**
+ *
+ * @author Filipe Emanuel
+ */
+public class Lampada {
+    String cor;
+    boolean estado;
+    String tipo;
+    double voltagem;
+    byte grauLuminosidade;
+
+    public Lampada() {
+    }
+    
+    public Lampada(String cor, boolean estado, String tipo, double voltagem, byte grauLuminosidade) {
+        this.cor = cor;
+        this.estado = estado;
+        this.tipo = tipo;
+        this.voltagem = voltagem;
+        this.grauLuminosidade = grauLuminosidade;
+    }
+    
+    public boolean ligar(){
+        return true;
+    }
+    
+    public boolean desligar(){
+        return false;
+    }
+    
+    public void estadoDaLuz(){
+        if(this.estado = true){
+            System.out.println("Luz Ligada");
+        } else {
+            System.out.println("Luz Desligada");
+        }
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getVoltagem() {
+        return voltagem;
+    }
+
+    public void setVoltagem(double voltagem) {
+        this.voltagem = voltagem;
+    }
+
+    public byte getGrauLuminosidade() {
+        return grauLuminosidade;
+    }
+
+    public void setGrauLuminosidade(byte grauLuminosidade) {
+        this.grauLuminosidade = grauLuminosidade;
+    }
+    
+    
+}
