67f5d9b1c7a8c7000a5b3c7562d0e35428695d34
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-12 16:03:21 +0200

    Adição de métodos get e set

--- CursoJava16h2019/src/correccaoExercAula08/exercicio15/Empregado.java
+++ CursoJava16h2019/src/correccaoExercAula08/exercicio15/Empregado.java
@@ -19,6 +19,29 @@ public class Empregado {
         this.sobrenome = sobrenome;
         this.salario = salario;
     }
-    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
     
 }
