ebce3946be68b1f6c09fe8059baf517146d30c3f
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-12 15:21:57 +0200

    Adição de classe Supermercado

---
+++ CursoJava16h2019/src/correccaoExercAula08/exercicio02/Supermercado.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package correccaoExercAula08.exercicio02;
+
+/**
+ *
+ * @author Filipe Emanuel
+ */
+public class Supermercado {
+    Produto produto;
+    byte quantidade;
+
+    public Supermercado() {
+    }
+    
+    public Supermercado(Produto produto, byte quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+    
+    
+}
