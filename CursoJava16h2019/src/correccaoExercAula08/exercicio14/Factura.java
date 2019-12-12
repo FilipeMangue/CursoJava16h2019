e34804d67d237b6c96819e9fa26ef59df0b28edb
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-12 15:57:53 +0200

    Adição de Método Get e Set

--- CursoJava16h2019/src/correccaoExercAula08/exercicio14/Factura.java
+++ CursoJava16h2019/src/correccaoExercAula08/exercicio14/Factura.java
@@ -22,4 +22,37 @@ public class Factura {
         this.precoPorItem = precoPorItem;
     }
 
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQtdComprada() {
+        return qtdComprada;
+    }
+
+    public void setQtdComprada(int qtdComprada) {
+        this.qtdComprada = qtdComprada;
+    }
+
+    public double getPrecoPorItem() {
+        return precoPorItem;
+    }
+
+    public void setPrecoPorItem(double precoPorItem) {
+        this.precoPorItem = precoPorItem;
+    }
+  
+    
 }
