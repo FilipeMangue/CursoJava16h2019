86e79643e00c886b573613b94889a97aa616613d
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-13 22:10:35 +0200

    Adding Methods

--- CursoJava16h2019/src/correccaoExercAula08/exercicio08/CadernoDeEnderecos.java
+++ CursoJava16h2019/src/correccaoExercAula08/exercicio08/CadernoDeEnderecos.java
@@ -23,5 +23,46 @@ public class CadernoDeEnderecos {
         this.data_nasc = data_nasc;
         this.endereco = endereco;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public byte getData_nasc() {
+        return data_nasc;
+    }
+
+    public void setData_nasc(byte data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
     
 }
