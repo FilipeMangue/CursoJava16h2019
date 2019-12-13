197c9a1804aa62911f82a8b027a1a2cc2ec04e81
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-13 22:15:23 +0200

    Method Get & Set

--- CursoJava16h2019/src/correccaoExercAula08/exercicio09/Livro.java
+++ CursoJava16h2019/src/correccaoExercAula08/exercicio09/Livro.java
@@ -21,6 +21,37 @@ public class Livro {
         this.codigo = codigo;
         this.preco = preco;
     }
-    
-    
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+       
 }
