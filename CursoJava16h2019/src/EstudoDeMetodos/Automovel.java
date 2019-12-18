13efd4140e1fabe72a4067758aa29c2a46acfdc2
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-18 15:05:28 +0200

    Classe Automovel

---
+++ CursoJava16h2019/src/EstudoDeMetodos/Automovel.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EstudoDeMetodos;
+
+/**
+ *
+ * @author Filipe Emanuel
+ */
+public class Automovel {
+   int velocidade;
+   String marca;
+   String cor;
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+   
+}
