02caf3cd4d66cbd584a378689ea32db6be100727
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-18 15:26:58 +0200

    Opercoes com metodos

--- CursoJava16h2019/src/correccaoExercHerancaPol/exer01/ContaAPrazoTeste.java
+++ CursoJava16h2019/src/correccaoExercHerancaPol/exer01/ContaAPrazoTeste.java
@@ -14,6 +14,17 @@ public class ContaAPrazoTeste {
        ContaAPrazo conta = new ContaAPrazo();
        
        conta.definirSaldoInicial(5000);
-       System.out.println("Saldo Actual: "+conta.getSaldo());
+       
+       System.out.println("Saldo Inicial: "+conta.getSaldo());
+       
+       conta.levantar(500);
+       
+       conta.depositar(100);
+       
+        System.out.println("Saldo depois de levanter 500.00 MZN e depositar 100.00 MZN: "+conta.getSaldo());
+        
+        conta.levantar(1200);
+        
+        System.out.println("Saldo depois de levanter + 1,200.00 MZN: "+conta.getSaldo());
     }
 }
