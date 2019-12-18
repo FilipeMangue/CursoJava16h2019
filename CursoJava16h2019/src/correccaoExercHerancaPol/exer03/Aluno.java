6b0c02f82a93aa517fb2cf693ef57e4e93da8670
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-18 16:31:10 +0200

    Metodos

---
+++ CursoJava16h2019/src/correccaoExercHerancaPol/exer03/Aluno.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package correccaoExercHerancaPol.exer03;
+
+/**
+ *
+ * @author Filipe Emanuel
+ */
+public class Aluno {
+    String estado;
+    double media;
+    
+    public String estadoAluno(){
+        if(media > 6)
+            return estado = "Aprovado";
+        if(media > 4 && media < 6)
+            return estado = "Candidato ao Exame";
+        if(media < 4)
+            return estado = "Reprovado";
+        return estado;
+                    
+    }
+    
+}
