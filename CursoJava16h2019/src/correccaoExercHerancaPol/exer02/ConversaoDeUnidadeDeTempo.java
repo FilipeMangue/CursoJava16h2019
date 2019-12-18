f639bb6ab88703dfba0708b746c6413776d54a7b
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-18 15:35:10 +0200

    Metodos de Conversao

--- CursoJava16h2019/src/correccaoExercHerancaPol/exer02/ConversaoDeUnidadeDeTempo.java
+++ CursoJava16h2019/src/correccaoExercHerancaPol/exer02/ConversaoDeUnidadeDeTempo.java
@@ -10,5 +10,31 @@ package correccaoExercHerancaPol.exer02;
  * @author Filipe Emanuel
  */
 public class ConversaoDeUnidadeDeTempo {
+    public static double minutoParaSegundo(double numero){
+        return numero * 60;
+    }
     
+    public static double horaParaMinutos(double numero){
+        return numero * 60;
+    }
+    
+    public static double diaParaHoras(double numero){
+        return numero * 24;
+    }
+    
+    public static double semanaParaDias(double numero){
+        return numero * 7;
+    }
+    
+    public static double mesParaDias(double numero){
+        return numero * 30;
+    }
+    
+    public static double anoParaDias(double numero){
+        return numero * 365.25;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Escolha a Opção do Seu Interesse:");
+    }
 }
