b2aa53ee197ad06bf52195d78a0917eb4bc6cb32
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-19 13:47:49 +0200

    Impressao da soma

--- CursoJava16h2019/src/correccaoExercHerancaPol/exer04/CalculadoraBase.java
+++ CursoJava16h2019/src/correccaoExercHerancaPol/exer04/CalculadoraBase.java
@@ -12,19 +12,19 @@ import java.util.Scanner;
  * @author Filipe Emanuel
  */
 public class CalculadoraBase {
-    public double soma(double num1, double num2){
+    public static double soma(double num1, double num2){
         return num1 + num2;
     }
     
-    public double subtracao(double num1, double num2){
+    public static double subtracao(double num1, double num2){
         return num1 - num2;
     }
     
-    public double multiplicacao(double num1, double num2){
+    public static double multiplicacao(double num1, double num2){
         return num1 * num2;
     }
     
-    public double divisao(double num1, double num2){
+    public static double divisao(double num1, double num2){
         if (num2 == 0) return num2 / num1;
         return num1 / num2;
     }
@@ -39,5 +39,8 @@ public class CalculadoraBase {
         
         System.out.print("Digite o num1: ");
         num2 = scan.nextDouble();
+        
+        System.out.println("Soma: "+soma(num1,num2));
+        
     }
 }
