edc4d12c05e54c520beeaa3c62269d77e25a6e11
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-19 13:38:50 +0200

    Metodo para subtracao

--- CursoJava16h2019/src/correccaoExercHerancaPol/exer04/CalculadoraBase.java
+++ CursoJava16h2019/src/correccaoExercHerancaPol/exer04/CalculadoraBase.java
@@ -13,4 +13,8 @@ public class CalculadoraBase {
     public double soma(double num1, double num2){
         return num1 + num2;
     }
+    
+    public double subtracao(double num1, double num2){
+        return num1 - num2;
+    }
 }
