73c4f9bf0fd0158d6d506084cd78b1e7ea954f3d
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-19 16:35:13 +0200

    Conclusao de exerc10

--- CursoJava16h2019/src/correccaoExercHerancaPol/exer10/Exerc10.java
+++ CursoJava16h2019/src/correccaoExercHerancaPol/exer10/Exerc10.java
@@ -5,10 +5,24 @@
  */
 package correccaoExercHerancaPol.exer10;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Filipe Emanuel
  */
 public class Exerc10 {
+    static double valor;
     
+    public static double prestacao(double valor){
+        return valor/5;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o valor do produto: ");
+        valor = scan.nextDouble();
+        
+        System.out.println("Valor a pagar em 5 prestacoes: "+prestacao(valor));
+    }
 }
