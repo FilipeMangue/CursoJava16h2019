7a2c488d412b29c853af9f71dfc67b04825095c6
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-19 13:41:46 +0200

    Metodo para divisao.

--- CursoJava16h2019/src/correccaoExercHerancaPol/exer04/CalculadoraBase.java
+++ CursoJava16h2019/src/correccaoExercHerancaPol/exer04/CalculadoraBase.java
@@ -21,4 +21,9 @@ public class CalculadoraBase {
     public double multiplicacao(double num1, double num2){
         return num1 * num2;
     }
+    
+    public double divisao(double num1, double num2){
+        if (num2 == 0) return num2 / num1;
+        return num1 / num2;
+    }
 }
