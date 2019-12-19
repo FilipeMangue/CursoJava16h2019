9331aee226a40ffeec4ad78b6ada26da9364c151
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-19 14:41:02 +0200

    Metodo para calculo da media

--- CursoJava16h2019/src/correccaoExercHerancaPol/exer07/Exerc07.java
+++ CursoJava16h2019/src/correccaoExercHerancaPol/exer07/Exerc07.java
@@ -16,7 +16,11 @@ public class Exerc07 {
     static double nota1;
     static double nota2;
     static double nota3;
-     
+    
+    public static double media(double nota1, double nota2, double nota3){
+        return (nota1 + nota2 + nota3)/3;
+    }
+    
     public static void main(String[] args) {
     }
 }
